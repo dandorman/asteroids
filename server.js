171ad0a0819216d2196b232822df44f6672c61eb
Dan Dorman <dan.dorman@gmail.com>
2011-08-19 12:54:14 -0600

    Recompile server.js for no.de box.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, express, port;
+  var app, cycle, express, io, next_thing_id, port, ship_colors, things;
   express = require('express');
   app = express.createServer();
   app.register('.coffee', require('coffeekup'));
@@ -17,5 +17,58 @@
   app.get('/', function(req, res) {
     return res.render('index.coffee');
   });
+  io = require('socket.io').listen(app);
   app.listen(port);
+  cycle = function(items) {
+    var index, length, _ref;
+    _ref = [0, items.length], index = _ref[0], length = _ref[1];
+    return {
+      next: function() {
+        return items[index++ % length];
+      }
+    };
+  };
+  ship_colors = cycle([
+    {
+      r: 0,
+      g: 255,
+      b: 0
+    }, {
+      r: 255,
+      g: 0,
+      b: 0
+    }, {
+      r: 0,
+      g: 0,
+      b: 255
+    }
+  ]);
+  things = {};
+  next_thing_id = 0;
+  io.sockets.on('connection', function(socket) {
+    var new_thing, tmp;
+    new_thing = {
+      id: ++next_thing_id,
+      x: 0,
+      y: 0,
+      color: ship_colors.next(),
+      maxSpeed: 3
+    };
+    things[next_thing_id] = new_thing;
+    tmp = {};
+    tmp[next_thing_id] = new_thing;
+    socket.broadcast.emit('add', tmp);
+    new_thing.yours = true;
+    socket.emit('add', things);
+    delete new_thing.yours;
+    return socket.on('update', function(data) {
+      var thing;
+      thing = things[data.id];
+      thing.x = data.position.x;
+      thing.y = data.position.y;
+      thing.angle = data.angle;
+      thing.velocity = data.velocity;
+      return socket.broadcast.emit('update', data);
+    });
+  });
 }).call(this);
